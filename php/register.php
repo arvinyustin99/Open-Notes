c334b7bf0d0f7445d48c18e44d5445ff66db28fb
arvinyustin99 <arvinyustin99>
2020-01-24 00:17:28 +0700

    query and response to DB

--- php/register.php
+++ php/register.php
@@ -5,6 +5,44 @@
 <?php
 	$dbconn = new DatabaseConnection("localhost", "adminON", "adminON", "opennotes");
 
-	// Check 
+	/* 	CHECK what type of request
+		1 -> validation
+		2 -> submission
 
+		Check what attribute if VALIDATION
+	*/
+	$form = array();
+	$form['status'] = 200;
+	if ($_POST["request_code"] == 1){
+		$type = "";
+
+		if ($_POST["type"] == "username"){$type = "username";}
+		else if ($_POST["type"] == "email"){$type = "email";}
+		else {
+			$form['status'] = 404;
+			$form = json_encode($form);
+			echo $form;
+			exit();
+		}
+
+		$result = $dbconn->query("SELECT username FROM users WHERE ".$type." = '".$_POST['attribute']."'");
+
+		$row = $result->fetch_assoc();
+
+		if ($result->num_rows > 0){
+			$form['status'] = 409;
+		}
+		$form = json_encode($form);
+		echo $form;
+	}
+	else if ($_POST["request_code"] == 0){
+		$result = $dbconn->query("INSERT INTO users (username, email, password) VALUES ('".$_POST['username']."', '".$_POST['email']."', '".$_POST['password']."');");
+
+		if ($result == false){
+			$form['status'] = 500;
+		}
+		$form = json_encode($form);
+		echo $form;
+
+	}
 ?>
